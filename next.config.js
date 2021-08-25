0d434bf90dbe7a3563eb3863794dcd73794cee6a
Hamza Miloud Amar <hamzamiloudamar@gmail.com>
2021-08-25 14:03:34 +0100

    first commit

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+  reactStrictMode: true,
+  eslint: {
+    dirs: ["src"], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
+  },
+};
